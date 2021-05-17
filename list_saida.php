63564dde6857e79f973940511b18a99f3f7e3c21
domingos filho <domingosjff@protonmail.com>
2021-05-17 20:50:53 -0300

    Aualizado -- 17/05/2021 --

--- list_saida.php
+++ list_saida.php
@@ -196,7 +196,7 @@ while($row_usuario = mysqli_fetch_assoc($resultado_usuario)){
                                 <tr>
                                     <th><?php echo $row_usuario['id']; ?></th>
                                     <td><?php echo $row_usuario['saida']; ?></td>
-                                    <td class="d-none d-sm-table-cell"><?php echo date('d/m/Y H:i:s', strtotime($row_usuario['created'])); ?></td>
+                                    <td><?php echo date('d/m/Y H:i:s', strtotime($row_usuario['created'])); ?></td>
 									
 									<td class="text-center">
                                         <span class="d-none d-md-block">
