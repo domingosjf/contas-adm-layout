29e6a7fe9009e7bf03187ccfdcce9774cbfdbd39
domingos filho <domingosjff@protonmail.com>
2021-05-16 21:04:41 -0300

    Atualizado -- 15/05/2021

--- index.php
+++ index.php
@@ -72,8 +72,23 @@ include_once("conexao.php");
 					
 					</li>
                     
-					<li><a href="#"><i class="fas fa-wallet"></i> Entrada</a></li>
-                    <li><a href="#"><i class="fas fa-money-check-alt"></i> Saída</a></li>
+					
+                   <li>
+					
+					<a href="#submenu3" data-toggle="collapse">
+                            <i class="fas fa-wallet"></i> Entrada
+                    </a>
+					
+					<ul id="submenu3" class="list-unstyled collapse">
+                            <li><a href="list_entrada.php"><i class="fas fa-wallet"></i> Entrada</a></li>
+                            
+                    </ul>
+					
+					
+					</li>
+                    
+
+				   <li><a href="#"><i class="fas fa-money-check-alt"></i> Saída</a></li>
                     <li><a href="#"><i class="fas fa-dollar-sign"></i> Saldo</a></li>
 					
 					
